2ab8705dd246caca78a654e1030ca5313c98db1b
sanchyy <vsanchez.gassull@gmail.com>
2019-10-01 11:38:48 +0200

    lab3

---
+++ haskell/lab3/P90677.hs
@@ -0,0 +1,44 @@
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f n []      = n
+myFoldl f n (x:xs)  = myFoldl (f n x) xs
+
+-- myFoldr :: (a -> b -> b) -> b -> [a] -> b
+-- myFoldr f _ []  = 
+-- myFoldr f n [x] = 
+
+-- myIterate :: (a -> a) -> a -> [a]
+
+
+-- myUntil :: (a -> Bool) -> (a -> a) -> a -> a
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f xs = [f x | x <- xs]
+-- myMap f []      = []
+-- myMap f (x:xs)  = [(f x)] ++  myMap f xs
+
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f xs = [x | x <- xs, f x]
+-- myFilter f [] = []
+-- myFilter f (x:xs)
+--  | f x         = x : myFilter f xs
+--  | otherwise   = myFilter f xs
+
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll f xs =   
+
+
+-- myAny :: (a -> Bool) -> [a] -> Bool
+-- lista copmresion
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _          = []
+myZip _ []          = []
+myZip (x:xs) (y:ys) = [(x,y)] ++ myZip xs ys
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f _ [] = []
+MyZipWith f [] _ = []
+MyZipWith f x y = myMap (uncurry f) (myZip x y)
+
