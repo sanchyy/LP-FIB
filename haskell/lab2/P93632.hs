8d1cc859e99d6a14fbfa89d92ecca1dc75031c29
sanchyy <vsanchez.gassull@gmail.com>
2019-10-05 19:11:59 +0200

    P96362

--- haskell/lab2/P93632.hs
+++ haskell/lab2/P93632.hs
@@ -1,13 +1,19 @@
-import Data.List
-
 eql :: [Int] -> [Int] -> Bool
-eql x y = 
+eql x y
+  | length x /= length y    = False
+  | otherwise               = and $ zipWith (==) x y
 
 prod :: [Int] -> Int
-prod = foldl (*) 1
+prod                        = foldl (*) 1
+
+prodOfEvens :: [Int] -> Int
+prodOfEvens                 = prod . filter even
+
+powersOf2 :: [Int]
+powersOf2                   = iterate (* 2) 1  
 
 scalarProduct :: [Float] -> [Float] -> Float
-scalarProduct x y = sum $ zipWith (*) x y
+scalarProduct x y           = sum $ zipWith (*) x y
 
 
 
