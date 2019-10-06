0063d5e5464e898d4965323f439b5151677c6328
sanchyy <vsanchez.gassull@gmail.com>
2019-10-06 13:33:52 +0200

    lab2 completed

---
+++ haskell/lab2/P93588.hs
@@ -0,0 +1,15 @@
+myMap :: (a -> b) -> [a] -> [b]
+myMap f xs = [f x | x <- xs]
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f xs = [x | x <- xs, f x]
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f xs ys = [f x y | (x,y) <- zip xs ys]
+
+thingify :: [Int] -> [Int] -> [(Int, Int)]
+thingify xs ys = [(x, y) | x <- xs, y <- ys, x `mod` y == 0 ]
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
--- haskell/lab3/P90677.hs
+++ haskell/lab3/P90677.hs
@@ -1,44 +1,45 @@
 myFoldl :: (a -> b -> a) -> a -> [b] -> a
-myFoldl f n []      = n
-myFoldl f n (x:xs)  = myFoldl (f n x) xs
-
-myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr f n []  = 
-myFoldr f n [x] = 
+myFoldl f n []          = n
+myFoldl f n (x:xs)      = myFoldl f (f n x) xs
 
 myIterate :: (a -> a) -> a -> [a]
-myIterate f n = [n] ++ myIterate f (f n) 
+myIterate f n           = [n] ++ myIterate f (f n) 
 
 -- myUntil :: (a -> Bool) -> (a -> a) -> a -> a
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f xs = [f x | x <- xs]
--- myMap f []      = []
--- myMap f (x:xs)  = [(f x)] ++  myMap f xs
+myMap f xs              = [f x | x <- xs]
+
+-- myMap f []           = []
+-- myMap f (x:xs)       = [(f x)] ++  myMap f xs
 
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter f xs = [x | x <- xs, f x]
--- myFilter f [] = []
+myFilter f xs           = [x | x <- xs, f x]
+
+-- myFilter f []        = []
 -- myFilter f (x:xs)
---  | f x         = x : myFilter f xs
---  | otherwise   = myFilter f xs
+--  | f x               = x : myFilter f xs
+--  | otherwise         = myFilter f xs
 
 
-myAll :: (a -> Bool) -> [a] -> Bool
-myAll f xs =   
+-- myAll :: (a -> Bool) -> [a] -> Bool
+-- myAll f xs           =    
 
 
 -- myAny :: (a -> Bool) -> [a] -> Bool
--- lista copmresion
 
 myZip :: [a] -> [b] -> [(a, b)]
-myZip [] _          = []
-myZip _ []          = []
-myZip (x:xs) (y:ys) = [(x,y)] ++ myZip xs ys
+myZip xs ys             = [(x,y) | x <- xs, y <- ys]
+
+-- myZip [] _           = []
+-- myZip _ []           = []
+-- myZip (x:xs) (y:ys)  = [(x,y)] ++ myZip xs ys
 
 myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-myZipWith f _ [] = []
-MyZipWith f [] _ = []
-MyZipWith f x y = myMap (uncurry f) (myZip x y)
+myZipWith f xs ys = [f x y | (x,y) <- zip xs ys]
+
+-- myZipWith f _ []     = []
+-- MyZipWith f [] _     = []
+-- MyZipWith f x y      = myMap (uncurry f) (myZip x y)
 
