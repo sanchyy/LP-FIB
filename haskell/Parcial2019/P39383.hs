2cd2737b614dc310987b204f5057c97cf99d65c0
sanchyy <vsanchez.gassull@gmail.com>
2019-10-29 12:36:32 +0100

    taylor mes eficient

--- haskell/Parcial2019/P39383.hs
+++ haskell/Parcial2019/P39383.hs
@@ -1,8 +1,8 @@
 exps :: Float -> [Float]
 exps n          = zipWith (/) pow fact
     where 
-        fact    = scanl (*) 1 $ iterate (+1) 1
-        pow     = [n ** i | i <- [1..]]
+        fact    = scanl (*)  1 $ iterate (+1) 1
+        pow     = iterate (*n) 1.0 
 
 exponencial :: Float -> Float -> Float
 exponencial x e = sum $ takeWhile (>= e) $ exps x 
