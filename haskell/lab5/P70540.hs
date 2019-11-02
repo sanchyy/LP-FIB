ab8a4f0bf4f33e3f2a3508a079468b2fe69c5243
sanchyy <vsanchez.gassull@gmail.com>
2019-11-02 11:47:43 +0100

    lab5 done

--- haskell/lab5/P70540.hs
+++ haskell/lab5/P70540.hs
@@ -7,5 +7,41 @@ eval1 (Sub x y)     = eval1 x - eval1 y
 eval1 (Mul x y)     = eval1 x * eval1 y
 eval1 (Div x y)     = eval1 x `div` eval1 y
 
+eval2 :: Expr -> Maybe Int
+eval2 (Val x)       = Just x
+eval2 (Add x y)     = do
+    ex <- eval2 x
+    ey <- eval2 y
+    Just(ex + ey)
+eval2 (Sub x y)     = do
+    ex <- eval2 x
+    ey <- eval2 y
+    Just(ex-ey)
+eval2 (Mul x y)     = do
+    ex <- eval2 x
+    ey <- eval2 y
+    Just(ex*ey)
+eval2 (Div x y)     = do
+    ex <- eval2 x
+    ey <- eval2 y
+    if ey == 0 then Nothing else return (div ex ey)
 
+eval3 :: Expr -> Either String Int
+eval3 (Val x)       = Right x
+eval3 (Add x y)     = do
+    ex <- eval3 x
+    ey <- eval3 y
+    return (ex + ey)
+eval3 (Sub x y)     = do
+    ex <- eval3 x
+    ey <- eval3 y
+    return(ex-ey)
+eval3 (Mul x y)     = do
+    ex <- eval3 x
+    ey <- eval3 y
+    return(ex*ey)
+eval3 (Div x y)     = do
+    ex <- eval3 x
+    ey <- eval3 y
+    if ey == 0 then Left "div0" else return (div ex ey)
 
