fe6ac36b2e4cce9aef3c9410a86bed47e80d8859
sanchyy <vsanchez.gassull@gmail.com>
2019-11-13 00:14:00 +0100

    comprehension

---
+++ python/P66679.py
@@ -0,0 +1,16 @@
+def my_map(f, L):
+    return [f(x) for x in L]
+
+
+def my_filter(f, L):
+    return [x for x in L if f(x)]
+
+
+def factors(n):
+    return [x for x in range(1,n+1) if n % x == 0]
+
+
+def triplets(n):
+    return 
+
+print(triplets(20))
