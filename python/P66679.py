218c1493594beb743e5f12356406493c115e0984
sanchyy <vsanchez.gassull@gmail.com>
2019-11-17 15:22:40 +0100

    P6679

--- python/P66679.py
+++ python/P66679.py
@@ -7,10 +7,12 @@ def my_filter(f, L):
 
 
 def factors(n):
-    return [x for x in range(1,n+1) if n % x == 0]
+    return [x for x in range(1, n+1) if n % x == 0]
 
 
 def triplets(n):
-    return 
-
-print(triplets(20))
+    return [(a, b, c)
+            for a in range(1, n+1)
+            for b in range(1, n+1)
+            for c in range(b, n+1)
+            if a * a + b * b == c * c]
