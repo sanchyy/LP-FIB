80c948fac3d44f9a5be56c11dc48aa8eca747a48
sanchy <vsanchez.gassull@gmail.com>
2020-01-09 22:09:11 +0100

    charts

--- practica/bot/bot.py
+++ practica/bot/bot.py
@@ -1,6 +1,7 @@
 from config import TOKEN
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+import networkx as nx
 import pickle
 
 class Bot:
@@ -36,29 +37,54 @@ class Bot:
 
     def author(self, bot, update):
         msg = '''
-    Autor: Víctor Sánchez Gassull
-        e-mail: victor.sanchez.gassull@est.fib.upc.edu
+Autor: Víctor Sánchez Gassull
+e-mail: victor.sanchez.gassull@est.fib.upc.edu
         '''
         bot.send_message(chat_id=update.message.chat_id, text=msg)
 
     def quiz(self, bot, update):
         self.id_enquesta = update.message.text[6:]  # remove string: "/quiz "
+        if len(self.id_enquesta) < 1:
+            bot.send_message(chat_id=update.message.chat_id, text='Usage: /Quiz identificador')
+            return
         try:
-           enquesta = pickle.load( open('../data/{}.dat'.format(self.id_enquesta)) ) 
+            self.enquesta = pickle.load(open('../data/{}.dat'.format(self.id_enquesta), 'rb')) 
         except:
-            bot.send_message(chat_id=update.message.chat_id, text="No hi ha cap enquesta amb aquest id")
-            pass
+            bot.send_message(chat_id=update.message.chat_id, text='No hi ha cap enquesta amb aquest identificador')
+            return
 
         # Do some loop waiting for the answer
+        print(nx.neighbors(self.id_enquesta))
+        print(self.enquesta)
+        sep = msg.split(':')
+
+        # actualitzar puntuacio
+        with open('../resultats/{}.json'.format(self.id_enquesta), 'r') as f:
+            data = json.load(f.readi())
+        data[question] += 1
+        with open('../resultats/{}.json'.format(self.id_enquesta), 'w') as f:
+            json.dump(f, data)
+
+        # op.update_grade(update.message.text)
         
 
     def bar(self, bot, update):
         question = update.message.text[5:]  # remove string "/bar " 
-        return "uwu"
+        with open('../resultats/{}.json', 'r') as f:
+            data = json.load(f.readi())
+        k, v = data.items() 
+        plt.bar(k,v)
+        plt.savefig('../static/{}_{}.png'.format(id_enquesta, question))
+        bot.send_photo(chat_id=update.message.chat_id, photo=open('../static/{}_{}.png'.format(id_enquesta, question)))
 
     def pie(self, bot, update):
         question = update.message.text[5:]  # remove string "/pie "
-        return id_pregunta
+        with open('../resultats/{}.json', 'r') as f:
+            data = json.load(f.readi())
+        k, v = data.items() 
+        plt.bar(k,v)
+        plt.savefig('../static/{}_{}.png'.format(id_enquesta, question))
+        bot.send_photo(chat_id=update.message.chat_id, photo=open('../static/{}_{}.png'.format(id_enquesta, question)))
 
     def report(self, bot, update):
         return "uwu"
